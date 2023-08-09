78a1da711873fd68b8f2bdaae01bf235819f1c2c
Tong Lin <wowawawoa@gmail.com>
2023-08-09 18:23:45 +0930

    feat: set global fonts using google fonts and set logo using fontawesome

---
+++ components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBrain } from "@fortawesome/free-solid-svg-icons";
+
+export const Logo = () => {
+  return (
+    <div className="text-3xl text-center py-4 font-heading">
+      AIBoostedBlogs
+      <FontAwesomeIcon icon={faBrain} className="text-2xl text-slate-400" />
+    </div>
+  );
+};
