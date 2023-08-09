437ae85f7921c9143a0ba2c443dcb6e8a39dbd8b
Tong Lin <wowawawoa@gmail.com>
2023-08-09 16:36:48 +0930

    feat: implementing the sidebar and styling with tailwind css

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,7 +1,18 @@
 export const AppLayout = ({ children }) => {
   return (
-    <div>
-      this is the app layout<div>{children}</div>
+    <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
+      <div className="flex flex-col text-white overflow-hidden">
+        <div className="bg-slate-800">
+          <div>log</div>
+          <div>cta button</div>
+          <div>tokens</div>
+        </div>
+        <div className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800">
+          list of posts
+        </div>
+        <div className="bg-cyan-800">user information - logout button</div>
+      </div>
+      <div className="">{children}</div>
     </div>
   );
 };
