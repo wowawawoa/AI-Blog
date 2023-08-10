4c4e55dafbad8829ae2da06bb9985d9e16200b83
Tong Lin <wowawawoa@gmail.com>
2023-08-10 14:12:10 +0930

    feat: refine the prompt and render the results

--- pages/api/generatePost.js
+++ pages/api/generatePost.js
@@ -6,14 +6,25 @@ export default async function handler(req, res) {
   });
   const openai = new OpenAIApi(config);
 
+  const topic = "Top 10 tips for dog owners";
+  const keywords =
+    "first-time dog owners, common dog health issues, best dog breeds";
+
   const response = await openai.createCompletion({
     model: "text-davinci-003",
     temperature: 0,
     max_tokens: 3600,
-    prompt: "Generate a blog post about owing dogs.",
+    prompt: `Write a long and detailed SEO-friendly blog post about ${topic}, that targets the following comma-separated keywords: ${keywords}. The content should be formatted in SEO-friendly HTML. The response must also include appropriate HTML title and meta description content. The return format must be stringified JSON in the following format: 
+    {
+      "postContent": post content here,
+      "title": title goes here,
+      "metaDescription": meta description goes here
+    }`,
   });
 
   console.log("api response", response);
 
-  res.status(200).json({ post: response.data.choices });
+  res
+    .status(200)
+    .json({ post: JSON.parse(response.data.choices[0]?.text.split("\n").join("")) });
 }
--- pages/post/new.js
+++ pages/post/new.js
@@ -1,14 +1,17 @@
 import { AppLayout } from "@/components/AppLayout";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { useState } from "react";
 
 export default function NewPost(props) {
   console.log("new post props", props);
+  const [postContent, setPostContent] = useState("");
   const handleClick = async () => {
     const response = await fetch("/api/generatePost", {
       method: "POST",
     });
     const json = await response.json();
-    console.log("result", json);
+    console.log("result", json.post.postContent);
+    setPostContent(json.post.postContent);
   };
 
   return (
@@ -17,6 +20,10 @@ export default function NewPost(props) {
       <button className="btn" onClick={handleClick}>
         Generate
       </button>
+      <div
+        className="max-w-screen-sm p-10"
+        dangerouslySetInnerHTML={{ __html: postContent }}
+      ></div>
     </div>
   );
 }
