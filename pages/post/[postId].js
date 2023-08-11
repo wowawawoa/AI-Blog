69d09274796c5eb97bc6b8fdc9e528615c84fd8b
Tong Lin <wowawawoa@gmail.com>
2023-08-11 12:26:52 +0930

    feat: load the post data for a particular post page

--- pages/post/[postId].js
+++ pages/post/[postId].js
@@ -1,7 +1,11 @@
-import { withPageAuthRequired } from '@auth0/nextjs-auth0';
-import { AppLayout } from '@/components/AppLayout';
+import { withPageAuthRequired, getSession } from "@auth0/nextjs-auth0";
+import { AppLayout } from "@/components/AppLayout";
+import clientPromise from "@/lib/mongodb";
+import { ObjectId } from "mongodb";
 
-export default function Post() {
+export default function Post(props) {
+  console.log("post props", props);
+
   return (
     <div>
       <h1>this is the post page</h1>
@@ -13,4 +17,35 @@ Post.getLayout = function getLayout(page, pageProps) {
   return <AppLayout {...pageProps}>{page}</AppLayout>;
 };
 
-export const getServerSideProps = withPageAuthRequired();
+export const getServerSideProps = withPageAuthRequired({
+  async getServerSideProps(context) {
+    const userSession = await getSession(context.req, context.res);
+    const client = await clientPromise;
+    const db = await client.db("ai-blog");
+    const users = await db.collection("users").findOne({
+      auth0Id: userSession.user.sub,
+    });
+    const post = await db.collection("posts").findOne({
+      _id: new ObjectId(context.params.postId),
+      userId: users._id,
+    });
+
+    if (!post) {
+      return {
+        redirect: {
+          destination: "/post/new",
+          permanent: false,
+        },
+      };
+    }
+
+    return {
+      props: {
+        postContent: post.postContent,
+        title: post.title,
+        metaDescription: post.metaDescription,
+        keywords: post.keywords,
+      },
+    };
+  },
+});
