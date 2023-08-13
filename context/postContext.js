f50099810d0e6e2914b9ca2a61b77c703d0955fb
Tong Lin <wowawawoa@gmail.com>
2023-08-13 18:13:26 +0930

    refactor: refactor posts context to use reducer instead of state

--- context/postContext.js
+++ context/postContext.js
@@ -1,37 +1,66 @@
-import { createContext, useCallback, useState } from "react";
+import { createContext, useCallback, useReducer, useState } from "react";
 
 const PostsContext = createContext({});
 
 export default PostsContext;
 
-export const PostsProvider = ({ children }) => {
-  const [posts, setPosts] = useState([]);
-  const [noMorePosts, setNoMorePosts] = useState(false);
-
-  const deletePost = useCallback((postId) => {
-    setPosts((value) => {
-      const newPosts = [];
-      value.forEach((post) => {
-        if (post._id !== postId) {
-          newPosts.push(post);
-        }
-      });
-      return newPosts;
-    });
-  }, []);
-
-  const setPostsFromSSR = useCallback((postsFromSSR = []) => {
-    // setPosts(postsFromSSR);
-    setPosts((value) => {
-      const newPosts = [...value];
-      postsFromSSR.forEach((post) => {
+function postReducer(state, action) {
+  switch (action.type) {
+    case "addPosts": {
+      const newPosts = [...state];
+      action.posts.forEach((post) => {
         const exists = newPosts.find((p) => p._id === post._id);
         if (!exists) {
           newPosts.push(post);
         }
       });
       return newPosts;
-    });
+    }
+    case "deletePost": {
+      const newPosts = [];
+      state.forEach((post) => {
+        if (post._id !== action.postId) {
+          newPosts.push(post);
+        }
+      });
+      return newPosts;
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+export const PostsProvider = ({ children }) => {
+  const [posts, dispatch] = useReducer(postReducer, []);
+  const [noMorePosts, setNoMorePosts] = useState(false);
+
+  const deletePost = useCallback((postId) => {
+    dispatch({ type: "deletePost", postId });
+    // setPosts((value) => {
+    //   const newPosts = [];
+    //   value.forEach((post) => {
+    //     if (post._id !== postId) {
+    //       newPosts.push(post);
+    //     }
+    //   });
+    //   return newPosts;
+    // });
+  }, []);
+
+  const setPostsFromSSR = useCallback((postsFromSSR = []) => {
+    dispatch({ type: "addPosts", posts: postsFromSSR });
+    // setPosts(postsFromSSR);
+    // setPosts((value) => {
+    //   const newPosts = [...value];
+    //   postsFromSSR.forEach((post) => {
+    //     const exists = newPosts.find((p) => p._id === post._id);
+    //     if (!exists) {
+    //       newPosts.push(post);
+    //     }
+    //   });
+    //   return newPosts;
+    // });
   }, []);
 
   const getPosts = useCallback(
@@ -51,16 +80,17 @@ export const PostsProvider = ({ children }) => {
         setNoMorePosts(true);
       }
 
-      setPosts((value) => {
-        const newPosts = [...value];
-        postsResult.forEach((post) => {
-          const exists = newPosts.find((p) => p._id === post._id);
-          if (!exists) {
-            newPosts.push(post);
-          }
-        });
-        return newPosts;
-      });
+      dispatch({ type: "addPosts", posts: postsResult });
+      // setPosts((value) => {
+      //   const newPosts = [...value];
+      //   postsResult.forEach((post) => {
+      //     const exists = newPosts.find((p) => p._id === post._id);
+      //     if (!exists) {
+      //       newPosts.push(post);
+      //     }
+      //   });
+      //   return newPosts;
+      // });
     },
     []
   );
